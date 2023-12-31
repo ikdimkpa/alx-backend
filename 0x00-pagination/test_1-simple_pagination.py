bc7b4c75f45bacef35e3931882cb493c9dde5f64
Ikenna Dimkpa <ikennadimkpa@gmail.com>
2023-12-31 10:34:29 +0000

    Debugging task 1

--- 0x00-pagination/test_1-simple_pagination.py
+++ 0x00-pagination/test_1-simple_pagination.py
@@ -37,8 +37,6 @@ class Server:
         assert isinstance(page, int) and page > 0
         assert isinstance(page_size, int) and page_size > 0
 
-        try:
-            start_index, end_index = index_range(page, page_size)
-            return self.dataset()[start_index:end_index]
-        except IndexError:
-            return []
+        start_index, end_index = index_range(page, page_size)
+
+        return self.dataset()[start_index:end_index]
